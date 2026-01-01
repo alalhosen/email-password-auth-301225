56d17c32dc93273d39fea2cb158b06e797000af7
Alal Hosen <alalhosen1992620120@gmail.com>
2026-01-01 15:36:40 +0600

    login & register setup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,16 @@
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
-    
+  const navlinks = (
+    <>
+      <li><NavLink to='/'>Home</NavLink></li>
+      <li><NavLink to='/login'>Login</NavLink></li>
+      <li><NavLink to='/register'>Register</NavLink></li>
+ 
+    </>
+  );
   return (
     <div>
-      <h1>Header</h1>
       <div className="navbar bg-base-100">
         <div className="navbar-start">
           <div className="dropdown">
@@ -26,43 +34,17 @@ const Header = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {navlinks}
+
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost bg-lime-500 rounded-full italic text-2xl">Email Auth</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
+            {navlinks}
             
-            
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
+            </ul>
         </div>
       </div>
     </div>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,22 @@
 const Register = () => {
   return (
     <div>
-      <h2 className="text 3xl">Please Register</h2>
+      <div className="mx-auto">
+        <h2 className="text 3xl underline">Please Register</h2>
+        <form>
+          <input
+            className="mb-2"
+            placeholder="email"
+            type="email"
+            name="email"
+            id=""
+          />
+          <br />
+          <input placeholder="password" type="password" name="password" id="" />
+          <br />
+          <input type="submit" value="Register" />
+        </form>
+      </div>
     </div>
   );
 };
